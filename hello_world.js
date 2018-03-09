7a6f5b5c70ab5dc76f6fe2eb65bcbcc2450ff15b
Mitchell-PC\Mitchell <mak427@gmail.com>
2018-03-09 19:01:29 +0100

    New Test

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+var GPIO = require('onoff').GPIO; //include onoff to interact with qpio pins
+var LED = new GPIO(4,'out');
+var blinkInterval = setInterval(blinkLED, 250);
+
+function blinkLED()
+{
+	if(LED.readSync() === 0)
+	{
+		LED.writeSync(1);
+	}
+	else
+	{
+		LED.writeSync(0);
+	}
+}
+
+function endBlink()
+{
+	clearInterval(blinkInterval);
+	LED.writeSync(0);
+	LED.unexport();
+}
+
+setTimeout(endBlink, 5000);
